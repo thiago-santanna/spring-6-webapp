58179182898f76adb4c3f8b0ad6d4ae6c8948fa1
thiago-santanna <uzmaki_thiago@outlook.com>
2024-04-01 16:29:56 -0300

    Initial project

---
+++ src/main/java/tss/webapp/spring6webapp/Spring6WebappApplication.java
@@ -0,0 +1,12 @@
+package tss.webapp.spring6webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6WebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6WebappApplication.class, args);
+	}
+}
